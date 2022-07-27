33ead20f3cf4914ae382d2ec2effa167a6143996
MaskeZen <91011274+MaskeZen945961@users.noreply.github.com>
2022-07-27 12:38:07 +0200

    Add files via upload

---
+++ Gen Paysafecard.py
@@ -0,0 +1,10 @@
+import random
+
+def main():
+    while True:
+        code = random.randint(1000000000000000,9999999999999999)
+        print (code)
+        
+main()
+
+#by MaskeZen https://github.com/MaskeZen945961
